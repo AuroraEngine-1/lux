066b43834ad4410a8d5324991223c250aa69ff11
Samuel Alonso <techiris09@gmail.com>
2020-11-05 17:17:17 +0000

    Updated file

--- Update.sh
+++ Update.sh
@@ -8,13 +8,4 @@ git commit -m "Updated file"
 git remote set-url origin https://AuroraEngine-1@github.com/AuroraEngine-1/lux.git
 git pull origin master
 git push --force origin master
-cd src
-cd docs
-cd html
-git init
-git add .
-git commit -m "Updated file"
-git remote set-url origin https://AuroraEngine-1@github.com/AuroraEngine-1/luxDocs.git
-git pull origin master
-git push --force origin master
 cd ..
