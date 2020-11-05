b71e9eb34b8f54f503e41bc55256a9d22da3eadd
Samuel Alonso <techiris09@gmail.com>
2020-11-05 17:19:19 +0000

    Updated file

--- Update.sh
+++ Update.sh
@@ -1,7 +1,7 @@
 git init
 git add .
 git commit -m "Updated file"
-git remote set-url origin https://AuroraEngine-1@github.com/AuroraEngine-1/lux.git
+git remote add origin https://AuroraEngine-1@github.com/AuroraEngine-1/lux.git
 git pull origin master
 git push --force origin master
 cd ..
